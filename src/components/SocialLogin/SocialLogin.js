e240bfa4f52c91a32e37d39243d31ffacf9bf196
akram hoque <oriniusakram511@gmail.com>
2022-08-20 21:08:11 +0600

    added social corre

--- src/components/SocialLogin/SocialLogin.js
+++ src/components/SocialLogin/SocialLogin.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import google from '../../../images/social/google.png';
 
 
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useNavigate } from 'react-router-dom';
-import Loading from '../../Shared/Loading/Loading';
+
 
 const SocialLogin = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, user,  error] = useSignInWithGoogle(auth);
     
     const navigate = useNavigate();
     
     let errorElement;
 
-    if(loading){
-        return <Loading></Loading>
-    }
 
     if (error) {
         errorElement = <p className='text-danger'>Error: {error?.message}</p>
@@ -38,7 +34,6 @@ const SocialLogin = () => {
                 <button
                     onClick={() => signInWithGoogle()}
                     className='btn btn-info w-50 d-block mx-auto my-2'>
-                    <img style={{ width: '30px' }} src={google} alt="" />
                     <span className='px-2'>Google Sign In</span>
                 </button>
                
